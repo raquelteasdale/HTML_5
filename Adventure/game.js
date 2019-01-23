4f8b2b1a91176e11d28f71253bb7f875c0ae2fa3
Unknown <raquel.t265@stu.nebo.edu>
2019-01-23 10:28:02 -0700

    Created code x2

---
+++ Adventure/game.js
@@ -0,0 +1,18 @@
+//single line comment
+/*
+
+multiline comment
+
+*/
+
+// document.write("<h1>I can't wait for <em>spring!</em></h1>");
+
+//alert("Warning! will Robinson...Warning!");
+
+//confirm("Do you like pokemon?");
+
+//prompt("What type of pokemon do you like?");
+
+var pc = prompt("What is your name?");
+
+confirm("So your name is "+pc);
